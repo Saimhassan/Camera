883548aeca7fac9c296c47c383bfebf76122ec5b
Saim <saimh355@gmail.com>
2020-02-06 15:43:49 +0500

    CAMERA HOTEL

--- app/src/main/java/com/example/death/MainActivity.java
+++ app/src/main/java/com/example/death/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.death;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        selectedImage = (ImageView)findViewById(R.id.person_image);
+        cameraBtn = (Button)findViewById(R.id.capture);
+        galleryBtn = (Button)findViewById(R.id.gallery);
 
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Camera Button is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Gallery Button is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
